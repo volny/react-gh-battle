1f2231c00d92c4e21fab49fd1626988d6cca4b6c
felix <felix@volny.co>
2016-03-04 15:35:04 +0800

    nested components and props

--- app/index.js
+++ app/index.js
@@ -2,16 +2,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var FriendsContainer = React.createClass({
   render: function() {
+    var name = 'Felix';
+    var friends = ['Richi', 'Jim', 'Louie'];
     return (
-      <div>Hello World</div>
+      <div>
+        <h3>Name: {name}</h3>
+        <ShowList names={friends} />
+      </div>
     )
   }
 })
 
+var ShowList = React.createClass({
+  render: function() {
+    var listItems = this.props.names.map(function(friend) {
+      return <li> {friend} </li>
+    });
+    return (
+      <div>
+        <h3>Friends</h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
-  <HelloWorld />,
+  <FriendsContainer />,
   document.getElementById('app')
 );
 
