370497be5591b902567cdbf6e2c4e3f4245cb0e9
felix <felix@volny.co>
2016-03-04 11:34:32 +0800

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     path: __dirname + 'dist/',
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    }
+  ]
 }
