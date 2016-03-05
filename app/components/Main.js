ac035077e4b93e6359ed590ae2a0fe82d935369e
felix <felix@volny.co>
2016-03-05 17:21:28 +0800

    hashHistory and IndexRoute

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,8 +3,7 @@ var React = require('react');
 var Main = React.createClass({
   render: function() {
     return (
-      <div>
-        Hello from Main
+      <div className='mainContainer'>
         {this.props.children}
       </div>
     )
