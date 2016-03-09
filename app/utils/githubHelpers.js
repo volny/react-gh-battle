b52a426b641b3ec7cb80610f17ce7354c6a04c40
felix <felix@volny.co>
2016-03-09 18:15:16 +0800

    calculate a score for the results

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -4,10 +4,37 @@ var id = 'YOUR_CLIENT_ID';
 var secret = 'YOUR_SECRET_ID';
 var param = '?client_id=' + id + '&client-secret=' + secret;
 
-// returns a promis
+// axios is promises-based, alle these functions return promises
 function getUserInfo (username) {
-  //axios.get('https://api.github.com/users/' + username + param);
-  return axios.get('https://api.github.com/users/' + username);
+  return axios.get('https://api.github.com/users/' + username + param);
+}
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function(prev, curr) {
+    return prev + curr.stargazers_count
+  }, 0);
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+  .then(getTotalStars)
+  .then(function(totalStars) {
+    return {
+      followers: player.followers,
+      totalStars: totalStars
+    }
+  })
+}
+
+function calculateScores (players) {
+    return [
+      players[0].followers * 3 +players[0].totalStars,
+      players[1].followers * 3 +players[1].totalStars
+    ]
 }
 
 var helpers = {
@@ -27,6 +54,18 @@ var helpers = {
     }).catch(function(err) {
       console.warn('Error in getPlayersInfo', err);
     })
+  },
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    // once all promises resolve ...
+    return axios.all([playerOneData, playerTwoData])
+    .then(calculateScores)
+    .catch(function (err) {
+      console.warn('Error in getPlayersInfo', err);
+    })
+
   }
 };
 
