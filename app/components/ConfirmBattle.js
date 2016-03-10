9c0ed23d55648b875c77f5ee0f779dd7a1f8119d
felix <felix@volny.co>
2016-03-10 11:24:45 +0800

    Loading component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,10 +4,11 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var Loading = require('./Loading');
 
 function ConfirmBattle(props) {
   return props.isLoading === true
-    ? <p> LOADING </p>
+    ? <Loading speed='300' text='Loading' />
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
     <h1>Confirm Players</h1>
     <div className='col-sm-8 col-sm-offset-2'>
